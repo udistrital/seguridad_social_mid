34513ad00bd62a543c75be9efa5b61129d239f6a
jcamilosarmientor <jcamilosarmientor@gmail.com>
2017-04-21 18:37:28 -0500

    Eliminados modelos incecesarios de otras aplicaciones como agora

--- back/predicado.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Predicado struct {
-	Id            int            `orm:"column(id);pk;auto"`
-	Nombre        string         `orm:"column(nombre)"`
-	Descripcion   string         `orm:"column(descripcion)"`
-
-}
