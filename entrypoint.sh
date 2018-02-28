03ccec214870c258488a519a30b31847ad3e0979
Fabio Parra <faparraf@gmail.com>
2018-02-28 15:43:50 -0500

    Ajustes CI

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+if [ -n "${PARAMETER_STORE:-}" ]; then
+  export SS_MID_API__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ss_mid_api/db/username --output text --query Parameter.Value)"
+  export SS_MID_API__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ss_mid_api/db/password --output text --query Parameter.Value)"
+fi
+
+exec ./main "$@"
