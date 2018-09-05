0a42a597814acd8368ddd1acfeed4ecc32fb7077
Juan Camilo Sarmiento Reyes <yhomii.cloud@gmail.com>
2018-09-04 21:45:36 -0500

    corrección en la función para filtrar las personas
    Se agrego el campo contratos, que trae todos los contratos activos del proveedor

--- controllers/incapacidades.go
+++ controllers/incapacidades.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/astaxie/beego"
 	"github.com/manucorporat/try"
 )
@@ -23,7 +25,7 @@ func (c *IncapacidadesController) URLMapping() {
 // @Failure 403
 // @router / [get]
 func (c *IncapacidadesController) GetPersonas() {
-	var proveedores, personaNatural, respuesta []map[string]interface{}
+	var proveedores, contratos, personaNatural, respuesta []map[string]interface{}
 	documento := c.GetString("documento")
 	try.This(func() {
 		if err := getJson("http://"+beego.AppConfig.String("administrativaService")+"/informacion_proveedor?"+
@@ -31,22 +33,42 @@ func (c *IncapacidadesController) GetPersonas() {
 			panic(err)
 		}
 
-		for index, proveedor := range proveedores {
+		for _, proveedor := range proveedores {
+			id := strconv.Itoa(int(proveedor["Id"].(float64)))
+			if err := getJson("http://"+beego.AppConfig.String("administrativaService")+"/contrato_general?"+
+				"query=Estado:true,Contratista:"+id+"&fields=Id,VigenciaContrato", &contratos); err != nil {
+				beego.Error("error en contrato general")
+				panic(err)
+			}
+
 			if err := getJson("http://"+beego.AppConfig.String("administrativaService")+"/informacion_persona_natural?"+
 				"limit=1&query=Id:"+proveedor["NumDocumento"].(string), &personaNatural); err != nil {
+				beego.Error("error en contrato informacion_persona_natural")
 				panic(err)
-			} else {
-				resp := map[string]interface{}{
-					"display":       proveedor["NomProveedor"],
-					"value":         proveedor["NumDocumento"],
-					"documento":     proveedor["NumDocumento"],
-					"id":            proveedor["Id"],
-					"nominas":       index,
-					"tipoDocumento": personaNatural[0]["TipoDocumento"].(map[string]interface{})["Abreviatura"],
-				}
-
-				respuesta = append(respuesta, resp)
 			}
+
+			var contratosPropios []map[string]interface{} // contratos de cada proveedor
+
+			for _, contrato := range contratos {
+				numeroContrato := contrato["Id"].(string)
+				vigenciaContrato := contrato["VigenciaContrato"].(float64)
+
+				temp := map[string]interface{}{
+					"NumeroContrato":   numeroContrato,
+					"VigenciaContrato": vigenciaContrato,
+				}
+				contratosPropios = append(contratosPropios, temp)
+			}
+
+			resp := map[string]interface{}{
+				"display":       proveedor["NomProveedor"],
+				"value":         proveedor["NumDocumento"],
+				"documento":     proveedor["NumDocumento"],
+				"contratos":     contratosPropios,
+				"tipoDocumento": personaNatural[0]["TipoDocumento"].(map[string]interface{})["Abreviatura"],
+			}
+
+			respuesta = append(respuesta, resp)
 		}
 		c.Data["json"] = respuesta
 
