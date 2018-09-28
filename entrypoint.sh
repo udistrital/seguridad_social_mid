7bed1df9bd9b53cf9204eeaf4ecf12f37371b981
fernandotower <fernandotower@gmail.com>
2018-09-28 12:06:01 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,9 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export SS_MID_API__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ss_mid_api/db/username --output text --query Parameter.Value)"
-  export SS_MID_API__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ss_mid_api/db/password --output text --query Parameter.Value)"
+  #No se conecta a base de datos
+  #export SS_MID_API__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ss_mid_api/db/username --output text --query Parameter.Value)"
+  #export SS_MID_API__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ss_mid_api/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
