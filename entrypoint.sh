303c96d279ddf39a0c65d9777069e6d1ada9ff03
Juan Camilo Sarmiento Reyes <yhomii.cloud@gmail.com>
2018-11-22 21:12:34 -0500

    Corrigiendo entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 set -u
 set -o pipefail
 
-if [ -n "${PARAMETER_STORE:-}" ]; then
+# if [ -n "${PARAMETER_STORE:-}" ]; then
   #No se conecta a base de datos
   #export SS_MID_API__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/ss_mid_api/db/username --output text --query Parameter.Value)"
   #export SS_MID_API__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/ss_mid_api/db/password --output text --query Parameter.Value)"
-fi
+# fi
 
 exec ./main "$@"
